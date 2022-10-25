1acfb3fd4e5080cae609e569c14008862e34f0af
Javier Medina <javier_j_medina@hotmail.com>
2022-10-24 21:57:45 -0300

    interfaz

---
+++ components/Footer.js
@@ -0,0 +1,91 @@
+import Link from 'next/link'
+import {
+    Box,
+    chakra,
+    Container,
+    SimpleGrid,
+    Stack,
+    Text,
+    VisuallyHidden,
+    useColorModeValue
+} from "@chakra-ui/react"
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa"
+
+
+const SocialButton = ({ children, label, href }) => {
+    return (
+        <chakra.button
+            bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+            rounded={"full"}
+            w={8}
+            h={8}
+            cursor={"pointer"}
+            as={"a"}
+            href={href}
+            display={"inline-flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            transition={"background 0.3s ease"}
+            target="_blank"
+            _hover={{
+                bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200")
+            }}
+        >
+            <VisuallyHidden>{label}</VisuallyHidden>
+            {children}
+        </chakra.button>
+    )
+}
+
+const ListHeader = ({ children }) => {
+    return (
+        <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+            {children}
+        </Text>
+    )
+}
+
+export default function Footer() {
+    return (
+        <Box
+            bg={useColorModeValue("gray.50", "gray.900")}
+            color={useColorModeValue("gray.700", "gray.200")}
+        >
+            <Container as={Stack} maxW={"6xl"} py={10}>
+                <SimpleGrid
+                    templateColumns={{ sm: "1fr 1fr", md: "2fr 1fr 1fr 2fr" }}
+                    spacing={8}
+                >
+                    <Stack spacing={6}>
+                        <Box>
+                            logo
+                        </Box>
+
+                        <Stack direction={"row"} spacing={6}>
+                            <SocialButton label={"Twitter"} href={"https://twitter.com/medinajavierj"}>
+                                <FaTwitter />
+                            </SocialButton>
+                            <SocialButton label={"LinkedIn"} href={"https://www.linkedin.com/in/javier-medina-783b721a9/"}>
+                                <FaLinkedin />
+                            </SocialButton>
+                            <SocialButton label={"Instagram"} href={"https://instagram.com/medinajavierj"}>
+                                <FaInstagram />
+                            </SocialButton>
+                            <SocialButton label={"GitHub"} href={"https://github.com/javiermedinaj?tab=repositories"}>
+                                <FaGithub />
+                            </SocialButton>
+                        </Stack>
+                    </Stack>
+                    <Stack align={"flex-start"}>
+                        <ListHeader>About</ListHeader>
+                        <Link href={"https://www.linkedin.com/in/javier-medina-783b721a9/"}>Contact</Link>
+                        <Link href="https://github.com/javiermedinaj?tab=repositories">My repositories</Link>
+                        <Link href="https://github.com/javiermedinaj/next-palta">Repository for this project</Link>
+                        <Link href="https://nextjs.org/">Framework</Link>
+                        <Link href={"https://chakra-ui.com/getting-started"}> Styles</Link>
+                    </Stack>
+                </SimpleGrid>
+            </Container>
+        </Box>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,89 @@
+import {
+    Box,
+    Flex,
+    Avatar,
+    HStack,
+    Link,
+    IconButton,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    useDisclosure,
+    useColorMode,
+    useColorModeValue,
+    Stack
+} from "@chakra-ui/react"
+import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon, TimeIcon } from "@chakra-ui/icons"
+
+
+export default function Navbar() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { colorMode, toggleColorMode } = useColorMode()
+    return (
+        <>
+            <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+                <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+                    <IconButton
+                        size={"md"}
+                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                        aria-label={"Open Menu"}
+                        display={{ md: "none" }}
+                        onClick={isOpen ? onClose : onOpen}
+                    />
+                    <Button ml={2} onClick={toggleColorMode}>
+                        {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                    </Button>
+                    <HStack spacing={8} alignItems={"center"}>
+                        <Box><Link href="/">
+                            Logo
+                        </Link></Box>
+                        <HStack
+                            as={"nav"}
+                            spacing={4}
+                            display={{ base: "none", md: "flex" }}
+                        >
+                            <Link href="/login">Login</Link>
+                            <Link href="dashboard"> Dashboard</Link>
+                            <Link href="/about"> About</Link>
+                        </HStack>
+                    </HStack>
+                    <Flex alignItems={"center"}>
+                        <Menu>
+                            <MenuButton
+                                as={Button}
+                                rounded={"full"}
+                                variant={"link"}
+                                cursor={"pointer"}
+                                minW={0}
+                            >
+                                <Avatar
+                                    size={"sm"}
+                                    src="images/dashboard.jpg"
+                                />
+                            </MenuButton>
+                            <MenuList>
+                                <MenuDivider />
+                                <MenuItem>Salir</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Flex>
+                </Flex>
+
+                {isOpen ? (
+                    <Box pb={4} display={{ md: "none" }}>
+                        <Stack as={"nav"} spacing={4}>
+                            <Link href="/login">Login</Link>
+                            <Link href="dashboard"> Dashboard</Link>
+                            <Link href="/about"> About</Link>
+                        </Stack>
+                    </Box>
+                ) : null}
+            </Box>
+
+
+        </>
+    )
+}
